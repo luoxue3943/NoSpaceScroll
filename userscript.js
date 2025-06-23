328207b09628b29a0aa6ac59acad2115e93f6620
珞雪 <luoxue3943@gmail.com>
2025-06-23 09:54:48 +0800

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,29 +1,26 @@
 // ==UserScript==
 // @name         No Space Scroll
 // @name:zh-CN   禁用空格翻页
-// @version      1.05
+// @version      1.06
 // @description  在页面加载初期即拦截全局的空格键（Space）和 Shift+Space 事件，仅在非输入框、非文本域及非可编辑区域触发时阻止默认的上下翻页行为，确保表单和富文本编辑区中的空格输入功能不受影响。
 // @author       珞雪
 // @match        *://*/*
-// @match        *://*.youtube.com
-// @match        *://*.bilibili.com
-// @exclude      *://*.bilibili.com/video/*
-// @exclude      *://*.bilibili.com/read/*
+// @exclude      *://*.bilibili.com/*/*
 // @exclude      *://*.youtube.com/*/*
-// @exclude      *://*.vimeo.com/*
-// @exclude      *://*.iqiyi.com/*
-// @exclude      *://*.youku.com/*
-// @exclude      *://*.tudou.com/*
-// @exclude      *://*.pptv.com/*
-// @exclude      *://*.v.qq.com/*
-// @exclude      *://*.mgtv.com/*
-// @exclude      *://*.sohu.com/*
-// @exclude      *://*.acfun.cn/*
-// @exclude      *://*.netflix.com/*
-// @exclude      *://*.primevideo.com/*
-// @exclude      *://*.hulu.com/*
-// @exclude      *://*.dailymotion.com/*
-// @exclude      *://*.twitch.tv/*
+// @exclude      *://*.vimeo.com/*/*
+// @exclude      *://*.iqiyi.com/*/*
+// @exclude      *://*.youku.com/*/*
+// @exclude      *://*.tudou.com/*/*
+// @exclude      *://*.pptv.com/*/*
+// @exclude      *://*.v.qq.com/*/*
+// @exclude      *://*.mgtv.com/*/*
+// @exclude      *://*.sohu.com/*/*
+// @exclude      *://*.acfun.cn/*/*
+// @exclude      *://*.netflix.com/*/*
+// @exclude      *://*.primevideo.com/*/*
+// @exclude      *://*.hulu.com/*/*
+// @exclude      *://*.dailymotion.com/*/*
+// @exclude      *://*.twitch.tv/*/*
 // @noframes
 // @run-at       document-start
 // @license      MIT
