faf47662a2c35f725eb1750c884ba6c1ab9b6461
珞雪 <luoxue3943@gmail.com>
2025-06-23 09:53:48 +0800

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No Space Scroll
 // @name:zh-CN   禁用空格翻页
-// @version      1.04
+// @version      1.05
 // @description  在页面加载初期即拦截全局的空格键（Space）和 Shift+Space 事件，仅在非输入框、非文本域及非可编辑区域触发时阻止默认的上下翻页行为，确保表单和富文本编辑区中的空格输入功能不受影响。
 // @author       珞雪
 // @match        *://*/*
